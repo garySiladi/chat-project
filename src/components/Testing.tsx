804bc9db04fb520c963e2133ce6a5501a132e756
pavol.kiss@akademiasovy.sk <pavol.kiss@akademiasovy.sk>
2019-06-01 21:01:19 +0200

    remove footer from testing

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { Footer } from "./Footer/Footer";
 
-export const Testing: FC<RouteComponentProps> = () => <Footer />;
+export const Testing: FC<RouteComponentProps> = () => <div />;
