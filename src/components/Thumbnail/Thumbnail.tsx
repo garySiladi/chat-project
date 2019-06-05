129d322eae6e670b3f914c6a833ffc9e9e631eb0
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-06-05 13:22:21 +0200

    done

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 export const Thumbnail: FC = ({ children }) => (
-  <div className="p-8 shadow w-full h-full flex-col flex justify-between font-lato">
+  <div className="p-8 text-left shadow w-full h-full flex-col flex justify-between font-lato">
     {children}
   </div>
 );
