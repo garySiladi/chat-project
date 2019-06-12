40046b5e34c80662fd9ce88986919a566db4c3ee
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-06-12 11:01:11 +0200

    ....

--- src/components/Thumbnail/Button.tsx
+++ src/components/Thumbnail/Button.tsx
@@ -8,7 +8,7 @@ interface IProps {
 export const Button: FC<IProps> = ({ children, className }) => (
   <button
     className={classnames(
-      "rounded-12 w-full h-40 font-black text-16 leading-24 hover:text-white hover:bg-black text-black",
+      "rounded-12 w-full h-40 font-black text-16 hover:text-white hover:bg-black text-black",
       className
     )}
   >
--- src/components/Thumbnail/ThumbnailArticle.tsx
+++ src/components/Thumbnail/ThumbnailArticle.tsx
@@ -24,7 +24,7 @@ export const ThumbnailArticle: FC<IProps> = ({
   <Thumbnail className="w-238 h-408 border border-solid border-lilac">
     <Image icon={icon} alt={alt} className="mb-8 w-full h-166" />
     <Date className="mb-4 h-12">{date}</Date>
-    <Name className="font-lato mb-8 h-50">{name}</Name>
+    <Name className="font-lato mb-8 leading-22">{name}</Name>
     <Description className="text-16 leading-22 mb-12 h-86">
       {description}
     </Description>
