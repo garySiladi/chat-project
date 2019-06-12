d2e98d18cb56a8370f8d8cbb1acf83caa0617704
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-06-12 11:14:43 +0200

    fixed bold text

--- src/components/Footer/Info.tsx
+++ src/components/Footer/Info.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export const Info = () => (
   <div className="mt-20 ml-24 h-40 text-10 font-lato">
-    <div>OZ CHAT - Centrum pre kreatívnu liečbu Arteterapiou</div>
+    <div className="font-bold">
+      OZ CHAT - Centrum pre kreatívnu liečbu Arteterapiou
+    </div>
     <div>IČO: 42407681</div>
     <div>Číslo účtu: VÚB SK13 0200 0000 0035 0237 4057</div>
   </div>
