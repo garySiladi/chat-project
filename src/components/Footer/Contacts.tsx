cb6a4781b945e6da5f6ec5d41f07e7b499a498af
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-06-13 11:44:49 +0200

    fixed font size

--- src/components/Footer/Contacts.tsx
+++ src/components/Footer/Contacts.tsx
@@ -9,13 +9,13 @@ export const Contacts = () => (
       <div className="mr-12 w-16">
         <Icon src={Phone} />
       </div>
-      <div className="font-lato text-10">+421 905 271 220</div>
+      <div className="font-lato text-12">+421 905 271 220</div>
     </div>
     <div className="flex items-center">
       <div className="mr-12 w-16">
         <Icon src={Email} />
       </div>
-      <a href="mailto:chatkosice@gmail.com" className="font-lato text-10">
+      <a href="mailto:chatkosice@gmail.com" className="font-lato text-12">
         chatkosice@gmail.com
       </a>
     </div>
