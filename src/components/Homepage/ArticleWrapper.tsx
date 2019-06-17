cb055c88b1898135027afbfd7f63dc503a51e9cb
TheStephaaan <43435775+Stephaaan@users.noreply.github.com>
2019-06-17 09:25:36 +0200

    Sr/article rendering update

--- src/components/Homepage/ArticleWrapper.tsx
+++ src/components/Homepage/ArticleWrapper.tsx
@@ -5,13 +5,13 @@ import { ThumbnailArticle } from "../Thumbnail/ThumbnailArticle";
 export interface IArticle {
   id: number;
   heading: string;
-  insight: string;
+  croppedInsight: string;
   titlePhoto: string;
 }
 export const ArticleWrapper: FC<IArticle> = ({
   id,
   heading,
-  insight,
+  croppedInsight,
   titlePhoto
 }) => (
   <div className="w-238 h-408 ml-0 mt-8 lg:mt-0 lg:ml-8">
@@ -20,7 +20,7 @@ export const ArticleWrapper: FC<IArticle> = ({
       name={heading}
       icon={`${backendHostname}/image/214/160/${titlePhoto}`}
       alt={heading}
-      description={insight}
+      description={croppedInsight}
     />
   </div>
 );
