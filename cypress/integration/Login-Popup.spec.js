da5598f805e321a884b72f08fd21954c89ae540d
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-06-19 15:06:36 +0200

    ddd

--- cypress/integration/Login-Popup.spec.js
+++ cypress/integration/Login-Popup.spec.js
@@ -11,17 +11,89 @@ describe("Login Tests", () => {
       .get("[data-cy=LoginError]");
   });
 
-  it("After Popup open click on Email", () => {
+  it("After Popup open click on Email input", () => {
     cy.visit("/PopupRegisterLogin")
       .focus()
-      .should("[data-cy=EmailInput]");
+      .get("[data-cy=EmailInput]");
   });
 
   it("After Popup open insert input into Email", () => {
     cy.visit("/PopupRegisterLogin");
     cy.get("[data-cy=EmailInput]")
       .type("jankohrasko")
-      .should("have.value", "Peter@gmail.com")
+      .should("have.value");
+  });
+
+  it("After bad insert 1st time into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("jankohrasko")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
       .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After bad insert 2nd time into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("peter@peter.peter")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After good insert into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("peter@gmail.com")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After Bad insert 3rd time into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("PETER@PETER.com")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After Bad insert 4th time into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("@gmail.com")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After Bad insert 5th time into Email input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("PeTeR@GmAiL.CoM")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("After Popup open click on Email input", () => {
+    cy.visit("/PopupRegisterLogin")
+      .focus()
+      .get("[data-cy=PasswordInput]");
+  });
+
+  it("After Bad insert 1st time into Password input press login", () => {
+    cy.visit("/PopupRegisterLogin")
+      .get("[data-cy=EmailInput]")
+      .type("samuraj123")
+      .should("have.value")
+      .click("[data-cy=LoginButton")
+      .get("[data-cy=PopupErrorInput]");
+  });
+
+  it("Press only login", () => {
+    cy.visit("/PopupRegisterLogin").get("[data-cy=PopupErrorInput]");
   });
 });
--- cypress/integration/Register-Popup.spec.js
+++ cypress/integration/Register-Popup.spec.js
@@ -1,6 +1,11 @@
-describe("Basic test", () => {
-  it("Visit homepage", () => {
-    cy.visit("/");
-    cy.get("[data-cy=header]").should("exist");
+describe("Register Tests", () => {
+  it("Open Popup of Login/Register", () => {
+    cy.visit("/PopupRegisterLogin");.click("[data-cy=RegisterClick]")
+  });
+  
+  it("Open Popup of Register itself", () => {
+    cy.visit("/PopupRegisterLogin").click("[data-cy=RegisterText]")
+    .visit("/RegisterPopUp");
+
   });
 });
